3f70cec381daf3940be7504d4708e16eacab4ce1
Briskheal <admin@briskheal.com>
2026-07-31 20:21:27 +0530

    fix: remove invalid Careers component from page causing 500 error

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -54,7 +54,6 @@ export default async function Home() {
             <ActivityGallery />
             <Publications />
             <FAQs />
-            <Careers />
             <Contact />
           </div>
         </main>
